73759f96107420de31e89e2941c4ac1139f78806
kaushalkoladiya <kaushalkoladiya123@gmail.com>
2020-07-11 00:08:48 +0530

    update home page

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-  <footer class="footer text-black">
-    <!-- Links -->
+  <!-- <footer class="footer text-black">
+    
     <div class="footer-seperator">
       <div class="content-lg container">
         <div class="row">
@@ -27,28 +27,126 @@
 
           </div>
         </div>
-        <!--// end row -->
+        
       </div>
     </div>
-    <!-- End Links -->
+    
 
-    <!-- Copyright -->
+    
     <div class="content container">
       <div class="row">
         <div class="col-xs-12 text-center color-white">
           <script>document.write(new Date().getFullYear());</script> &copy; Copyright <strong>destov International</strong> All rights reserved</div>
         </div>
-        <!-- <div class="col-xs-6 text-right">
-          <p class="margin-b-0"><a class="color-base fweight-700"
-              href="http://destov.com">Destov</a>
-             Theme Powered by: <a
-              class="color-base fweight-700" href="http://www.keenthemes.com/">KeenThemes.com</a></p>
-        </div> -->
       </div>
       
     </div>
     
-  </footer>
+  </footer> -->
+
+<footer class="footer">
+
+  <div class="footer-seperator">
+    <div class="content-lg container">
+      <div class="row">
+        <div class="col-sm-3 sm-margin-b-50">
+          <!-- List -->
+          <ul class="list-unstyled footer-list text-center">
+            <h2 class="text-capitalization" style="color:white">HOW TO REACH US</h2>
+            <div class="container"  style="color:white; display:contents">
+            
+              <div class="margin-b-10" style="display: -webkit-inline-box; text-align: center;">
+                <div>
+                  <i class="fa fa-lg fa-tag fa-map-marker margin-r-10"></i>
+                </div>
+                <div>
+                  Shop No.04, Olive Excel<br>Seawoods, Sector-42<br>New Mumbai - 400 706
+                </div>
+              </div><br>
+              <div class="margin-b-10" style="display: -webkit-inline-box; text-align: center;">
+                <div>
+                  <strong><i class="fa fa-lg fa-tag fa-phone margin-r-10"></i></strong>
+                </div>
+                <div>+91 8082087255</div>
+              </div><br>
+              <div class="margin-b-10" style="display: -webkit-inline-box; text-align: center;">
+                <div>
+                  <strong><i class="fa fa-lg fa-tag fa-envelope margin-r-10"></i></strong>
+                </div>
+                <div>info@destov.com</div>
+              </div><br>
+              <div class="margin-b-10 margin-l-20" style="display: -webkit-inline-box; text-align: center;">
+                <a class="footer-list-link" href="https://www.facebook.com/destovinternational"><i class="margin-r-10 fa fa-lg fa-facebook"></i></a>
+                <a class="footer-list-link" href="https://www.facebook.com/destovinternational"><i class="margin-r-10 fa fa-lg fa-tag fa-twitter"></i></a>
+                <a class="footer-list-link" href="https://twitter.com/destovofficial"><i class="margin-r-10 fa fa-lg fa-tag fa-linkedin"></i></a>
+                <a class="footer-list-link" href="https://www.facebook.com/destovinternational"><i class="margin-r-10 fa fa-lg fa-tag fa-instagram"></i></a>
+              </div>
+
+            </div>
+          </ul>
+          <!-- End List -->
+        </div>
+        <div class="col-sm-4 sm-margin-b-30">
+          <!-- List -->
+          <ul class="list-unstyled footer-list text-center">
+            <h2 style="color: white">Useful Links</h2>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="index.php">Home</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="about.php">About Us</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="products.php">Our Products</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="blog.php">Blog</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="contact.php">Contact Us</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="#">Terms And Conditions</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="#">Privacy Policy</a>
+            </li>
+            <li class="margin-b-10"><a class="footer-list-link" style="color: white" href="#">Trams Of Use</a>
+            </li>
+          </ul>
+          <!-- End List -->
+        </div>
+        <div class="col-sm-5 sm-margin-b-30 text-center">
+          <h2 class="color-white">GET LATEST MARKET UPDATES</h2>
+          <p style="color: white; font-weight:500">
+            Subscribe to get latest Market Updates, Our News Letter and Exclusive Deals directly to your in your main-box.
+          </p>
+          <input type="text" class="form-control footer-input margin-b-20" placeholder="Name" required>
+          <button type="submit" class="btn-theme btn-theme-sm btn-base-bg text-uppercase">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="content text-black bg-color-white container-fluid">
+    <div style="display: flex; justify-content: space-between;">
+      <div>
+        Copyright &copy;
+        <script>document.write(new Date().getFullYear());</script> <strong>Destov International</strong> All rights
+        reserved
+      </div>
+      <div>
+        Designed and Developed by <strong>Destov Tech</strong>
+      </div>
+    </div>
+  </div>
+  
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
 
   <!-- Back To Top -->
   <a href="javascript:void(0);" class="js-back-to-top back-to-top">Top</a>
--- index.php
+++ index.php
@@ -145,28 +145,40 @@ $slides = mysqli_query($cn, $query);
       <img class="img-responsive" src="img/970x970/01.jpg" alt="Content Image">
     </div>
   </div>
-  <!-- End About us -->
-
-  <!-- Work -->
+  
   <div class="bg-color-sky-light overflow-h">
     <div class="content-lg container">
       <div class="row margin-b-40">
         <div class="col-sm-6">
           <h2>Products</h2>
-          <!-- <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incididunt ut laboret dolore
-            magna aliqua enim minim veniam exercitation</p> -->
         </div>
       </div>
-      <!--// end row -->
+      
 
-      <!-- Masonry Grid -->
       <div class="masonry-grid">
         <div class="masonry-grid-sizer col-xs-6 col-sm-6 col-md-1"></div>
-        <div class="masonry-grid-item col-xs-12 col-sm-6 col-md-8">
+     
+        <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
           <!-- Work -->
+          <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".2s">
+            <div class="work-overlay">
+              <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
+            </div>
+            <div class="work-content">
+              <h3 class="color-white margin-b-5">Clean Design</h3>
+              <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
+            </div>
+            <a class="content-wrapper-link"
+              href="product.php"></a>
+          </div>
+          <!-- End Work -->
+        </div>
+        
+        <div class="masonry-grid-item col-xs-12 col-sm-6 col-md-8">
+
           <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".1s">
             <div class="work-overlay">
-              <img class="full-width img-responsive" src="img/800x400/01.jpg" alt="Portfolio Image">
+              <img class="full-width img-responsive" src="img/970x647/02.jpg" alt="Portfolio Image">
             </div>
             <div class="work-content">
               <h3 class="color-white margin-b-5">Product</h3>
@@ -175,28 +187,14 @@ $slides = mysqli_query($cn, $query);
             <a class="content-wrapper-link"
               href="product.php"></a>
           </div>
-          <!-- End Work -->
-        </div>
-        <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
-          <!-- Work -->
-          <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".2s">
-            <div class="work-overlay">
-              <img class="full-width img-responsive" src="img/397x400/01.jpg" alt="Portfolio Image">
-            </div>
-            <div class="work-content">
-              <h3 class="color-white margin-b-5">Clean Design</h3>
-              <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
-            </div>
-            <a class="content-wrapper-link"
-              href="product.php"></a>
-          </div>
-          <!-- End Work -->
-        </div>
+
+        </div>   
+
         <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
           <!-- Work -->
           <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".3s">
             <div class="work-overlay">
-              <img class="full-width img-responsive" src="img/397x300/01.jpg" alt="Portfolio Image">
+              <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
             </div>
             <div class="work-content">
               <h3 class="color-white margin-b-5">Clean Design</h3>
@@ -207,11 +205,51 @@ $slides = mysqli_query($cn, $query);
           </div>
           <!-- End Work -->
         </div>
+
+        <div class="masonry-grid-item col-xs-12 col-sm-6 col-md-8">
+          <!-- Work -->
+          <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".1s">
+              <div class="work-overlay">
+                  <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
+              </div>
+              <div class="work-content">
+                  <h3 class="color-white margin-b-5">Art Of Coding</h3>
+                  <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
+              </div>
+              <a class="content-wrapper-link" href="#"></a>
+          </div>
+          <!-- End Work -->
+        </div>
+
+        <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
+          <!-- Work -->
+          <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".1s">
+            <div class="work-overlay">
+                <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
+            </div>
+            <div class="work-content">
+                <h3 class="color-white margin-b-5">Clean Fruit</h3>
+                <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
+            </div>
+            <a class="content-wrapper-link" href="#"></a>
+          </div>
+          <div class="work">
+            <div class="work-overlay">
+              <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
+            </div>
+            <div class="work-content">
+              <h3 class="color-white margin-b-5">Show All</h3>
+            </div>
+            <a class="content-wrapper-link" href="prodcuts.php"></a>
+          </div>
+          <!-- End Work -->
+        </div>
+
         <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
           <!-- Work -->
           <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".4s">
             <div class="work-overlay">
-              <img class="full-width img-responsive" src="img/397x300/02.jpg" alt="Portfolio Image">
+              <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
             </div>
             <div class="work-content">
               <h3 class="color-white margin-b-5">Clean Design</h3>
@@ -222,11 +260,12 @@ $slides = mysqli_query($cn, $query);
           </div>
           <!-- End Work -->
         </div>
+
         <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
           <!-- Work -->
           <div class="work wow fadeInUp" data-wow-duration=".3" data-wow-delay=".5s">
             <div class="work-overlay">
-              <img class="full-width img-responsive" src="img/397x300/03.jpg" alt="Portfolio Image">
+              <img class="full-width img-responsive" src="img/970x647/01.jpg" alt="Portfolio Image">
             </div>
             <div class="work-content">
               <h3 class="color-white margin-b-5">Clean Design</h3>
@@ -238,201 +277,59 @@ $slides = mysqli_query($cn, $query);
           <!-- End Work -->
         </div>
        
-        <div class="masonry-grid-item col-xs-12 col-sm-6 col-md-8">
-          <!-- Work -->
-          <div class="work">
-              <div class="work-overlay">
-                  <img class="full-width img-responsive" src="img/800x400/02.jpg" alt="Portfolio Image">
-              </div>
-              <div class="work-content">
-                  <h3 class="color-white margin-b-5">Art Of Coding</h3>
-                  <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
-              </div>
-              <a class="content-wrapper-link" href="#"></a>
-          </div>
-          <!-- End Work -->
-        </div>
-        <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-4">
-          <!-- Work -->
-          <div class="work">
-            <div class="work-overlay">
-                <img class="full-width img-responsive" src="img/397x400/02.jpg" alt="Portfolio Image">
-            </div>
-            <div class="work-content">
-                <h3 class="color-white margin-b-5">Clean Fruit</h3>
-                <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
-            </div>
-            <a class="content-wrapper-link" href="#"></a>
-          </div>
-          <div class="work">
-            <div class="work-overlay">
-              <img class="full-width img-responsive" src="img/397x300/02.jpg" alt="Portfolio Image">
-            </div>
-            <div class="work-content">
-              <h3 class="color-white margin-b-5">Show All</h3>
-            </div>
-            <a class="content-wrapper-link" href="prodcuts.php"></a>
-          </div>
-          <!-- End Work -->
-        </div>
+          
+       
       </div>
       <!-- End Masonry Grid -->
     </div>
   </div>
 
-  <!-- <div class="bg-color-sky-light">
-    <div class="content-md container">
-        
-        <div class="masonry-grid" style="position: relative; height: 385.524px;">
-            <div class="masonry-grid-sizer col-xs-6 col-sm-6 col-md-6"></div>
-            <div class="masonry-grid-item col-xs-12 col-sm-6 col-md-6" style="position: absolute; left: 0%; top: 0px;">
-                
-                <div class="work">
-                    <div class="work-overlay">
-                        <img class="full-width img-responsive" src="img/595x483/01.jpg" alt="Portfolio Image">
-                    </div>
-                    <div class="work-content">
-                        <h3 class="color-white margin-b-5">Art Of Coding</h3>
-                        <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
-                    </div>
-                    <a class="content-wrapper-link" href="#"></a>
-                </div>
-                
-            </div>
-            <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-6" style="position: absolute; left: 50%; top: 0px;">
-                
-                <div class="work">
-                    <div class="work-overlay">
-                        <img class="full-width img-responsive" src="img/595x240/01.jpg" alt="Portfolio Image">
-                    </div>
-                    <div class="work-content">
-                        <h3 class="color-white margin-b-5">Clean Design</h3>
-                        <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
-                    </div>
-                    <a class="content-wrapper-link" href="#"></a>
-                </div>
-                
-            </div>
-            <div class="masonry-grid-item col-xs-6 col-sm-6 col-md-6" style="position: absolute; left: 50%; top: 192px;">
-                
-                <div class="work">
-                    <div class="work-overlay">
-                        <img class="full-width img-responsive" src="img/595x240/02.jpg" alt="Portfolio Image">
-                    </div>
-                    <div class="work-content">
-                        <h3 class="color-white margin-b-5">Amazing Support</h3>
-                        <p class="color-white margin-b-0">Lorem ipsum dolor sit amet consectetur adipiscing</p>
-                    </div>
-                    <a class="content-wrapper-link" href="#"></a>
-                </div>
-                
-            </div>
-        </div>
-        
-    </div>
-  </div> -->
-  <!-- End Work -->
-  
   <!-- Service -->
   <div class="bg-color-sky-light" data-auto-height="true">
     <div class="content-lg container">
+      <h2>Certificates</h2>
       <div class="row row-space-1 margin-b-2">
-        <div class="col-sm-4 sm-margin-b-2">
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3 sm-margin-b-2">
           <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".3s">
-            <div class="service" data-height="height">
-              <div class="service-element">
-                <i class="service-icon icon-chemistry"></i>
-              </div>
-              <div class="service-info">
-                <h3>Art Of Coding</h3>
-                <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat
-                  dolor</p>
-              </div>
-             
-            </div>
+              <img class="img-responsive" src="./img/certificates/04.png" alt="">
           </div>
         </div>
-        <div class="col-sm-4 sm-margin-b-2">
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3 sm-margin-b-2">
           <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".2s">
-            <div class="service" data-height="height">
-              <div class="service-element">
-                <i class="service-icon icon-screen-tablet"></i>
-              </div>
-              <div class="service-info">
-                <h3>Responsive Design</h3>
-                <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat
-                  dolor</p>
-              </div>
-             
-            </div>
+            <img class="img-responsive" src="./img/certificates/05.png" alt="">
           </div>
         </div>
-        <div class="col-sm-4">
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3">
           <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".1s">
-            <div class="service" data-height="height">
-              <div class="service-element">
-                <i class="service-icon icon-badge"></i>
-              </div>
-              <div class="service-info">
-                <h3>Feature Reach</h3>
-                <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat
-                  dolor</p>
-              </div>
-             
-            </div>
+            <img class="img-responsive" src="./img/certificates/06.png" alt="">
+          </div>
+        </div>
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3 sm-margin-b-2">
+          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".3s">
+              <img class="img-responsive" src="./img/certificates/01.png" alt="">
           </div>
         </div>
       </div>
-      <!--// end row -->
 
-      <div class="row row-space-1">
-        <div class="col-sm-4 sm-margin-b-2">
-          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".4s">
-            <div class="service" data-height="height">
-              <div class="service-element">
-                <i class="service-icon icon-notebook"></i>
-              </div>
-              <div class="service-info">
-                <h3>Useful Documentation</h3>
-                <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat
-                  dolor</p>
-              </div>
-             
-            </div>
+      <div class="row row-space-1 margin-b-2">
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3 sm-margin-b-2">
+          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".2s">
+            <img class="img-responsive" src="./img/certificates/02.png" alt="">
           </div>
         </div>
-        <div class="col-sm-4 sm-margin-b-2">
-          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".5s">
-            <div class="service" data-height="height">
-              <div class="service-element">
-                <i class="service-icon icon-speedometer"></i>
-              </div>
-              <div class="service-info">
-                <h3>Fast Delivery</h3>
-                <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat
-                  dolor</p>
-              </div>
-             
-            </div>
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3 sm-margin-b-2">
+          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".2s">
+            <img class="img-responsive" src="./img/certificates/07.png" alt="">
           </div>
         </div>
-        <div class="col-sm-4">
-          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".6s">
-            <div class="service" data-height="height">
-              <div class="service-element">
-                <i class="service-icon icon-badge"></i>
-              </div>
-              <div class="service-info">
-                <h3>Free Plugins</h3>
-                <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat
-                  dolor</p>
-              </div>
-             
-            </div>
+        <div style="padding: 30px 60px; box-sizing: border-box" class="col-sm-3">
+          <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".1s">
+            <img class="img-responsive" src="./img/certificates/03.png" alt="">
           </div>
         </div>
       </div>
-      <!--// end row -->
+
+    
     </div>
   </div>
   <!-- End Service -->
