65e6c17c72684b836e3a21ced1d3e227400e326a
kaushalkoladiya <kaushalkoladiya123@gmail.com>
2020-07-14 07:54:05 +0530

    chat icon

--- includes/footer.php
+++ includes/footer.php
@@ -90,6 +90,20 @@
   
 </footer>
 <script type="text/javascript">
+  (function () {
+    var options = {
+      facebook: "113975870339215", // Facebook page ID
+      whatsapp: "+91 80820 87255", // WhatsApp number
+      call_to_action: "Message us", // Call to action
+      button_color: "#129BF4", // Color of button
+      position: "left", // Position may be 'right' or 'left'
+      order: "facebook,whatsapp", // Order of buttons
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+  })();
   function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
   }
