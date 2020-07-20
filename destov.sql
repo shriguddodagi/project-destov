5a249c5ed577a9393a2424d0285e6ea2068fe1de
kaushalkoladiya <kaushalkoladiya123@gmail.com>
2020-07-20 17:40:09 +0530

    Update DB

--- destov.sql
+++ destov.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2020 at 06:02 PM
+-- Generation Time: Jul 20, 2020 at 02:07 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -51,16 +51,18 @@ CREATE TABLE `blogs` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `description` text NOT NULL,
-  `image` varchar(100) NOT NULL
+  `image` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `blogs`
 --
 
-INSERT INTO `blogs` (`id`, `title`, `description`, `image`) VALUES
-(1, 'New blog', 'This is sort description', 'storage/1594381971carbon (3).png'),
-(2, 'Second Blog', 'No description required', 'storage/1594382074carbon.png');
+INSERT INTO `blogs` (`id`, `title`, `description`, `image`, `created_at`) VALUES
+(3, 'skdfnkdsjnkdfmg', 'lkjdsmfdsklgm', 'storage/159498190401.jpg', '2020-07-20 16:38:11'),
+(4, 'lkjgmflkd', 'lmsdgklmv', 'storage/1594981917logo2.JPG', '2020-07-20 16:38:11'),
+(5, 'skadfnds', '<p>klsdfmkdslfnm</p>\r\n\r\n<p>sjdfhdsk</p>\r\n\r\n<p>alsdkf</p>\r\n', 'storage/1595091580download.png', '2020-07-20 16:38:11');
 
 -- --------------------------------------------------------
 
@@ -79,8 +81,9 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `terms`) VALUES
-(1, 'qwertyuiop', 'zxcvbnmsadfghjkl34567jklm;tg'),
-(2, 'xsedignv', 'ddbui');
+(1, 'qwertyuiop', '<p>With edited text</p>\r\n\r\n<p>and other also</p>\r\n'),
+(2, 'xsedignv', 'ddbui'),
+(3, 'new category', 'sdkfjsdklfl');
 
 -- --------------------------------------------------------
 
@@ -104,8 +107,29 @@ CREATE TABLE `feedbacks` (
 
 INSERT INTO `feedbacks` (`id`, `name`, `email`, `phone`, `message`, `status`, `created_at`) VALUES
 (1, 'skdnf', 'kls@ksdjf.sdflj', '48372238947382', 'skfjmdskf', '0', '2020-07-15 20:50:53'),
-(2, 'skdnf', 'kls@ksdjf.sdflj', '48372238947382', 'skfjmdskf', '1', '2020-07-15 20:50:53'),
-(3, 'skdnf', 'kls@ksdjf.sdflj', '48372238947382', 'skfjmdskf', '0', '2020-07-15 20:50:53');
+(2, 'skdnf', 'kls@ksdjf.sdflj', '48372238947382', 'skfjmdskf', '0', '2020-07-15 20:50:53'),
+(3, 'skdnf', 'kls@ksdjf.sdflj', '48372238947382', 'skfjmdskf', '0', '2020-07-15 20:50:53'),
+(4, 'sdjfg', 'kn@slsdkjf.asdj', '349857948392', 'dsifjkdm', '0', '2020-07-17 09:52:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `productId` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `image`, `productId`) VALUES
+(2, 'storage/1595243353download.png', 12),
+(5, 'storage/1595244241carbon (4).png', 12);
 
 -- --------------------------------------------------------
 
@@ -121,6 +145,7 @@ CREATE TABLE `inquiries` (
   `email` varchar(50) NOT NULL,
   `phone` varchar(100) NOT NULL,
   `message` longtext NOT NULL,
+  `product` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `mode` varchar(5) DEFAULT 'open'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -129,19 +154,20 @@ CREATE TABLE `inquiries` (
 -- Dumping data for table `inquiries`
 --
 
-INSERT INTO `inquiries` (`id`, `name`, `position`, `company`, `email`, `phone`, `message`, `created_at`, `mode`) VALUES
-(124, 'dfnskm', '', '', 'hjfdsk@hfjdkf.urefjdsks', '4538920', 'gfhdsjk', '2020-07-10 00:00:00', 'open'),
-(125, 'hgdfjk', '', '', 'dfuhsj@gdhfsj.ergufdjs', '348920', 'fdhjsk', '2020-07-10 00:00:00', 'open'),
-(126, 'hgdfjk', '', '', 'dfuhsj@gdhfsj.ergufdjs', '348920', 'fdhjsk', '2020-07-10 00:00:00', 'close'),
-(127, 'hgdfjk', '', '', 'dfuhsj@gdhfsj.ergufdjs', '348920', 'fdhjsk', '2020-07-10 00:00:00', 'open'),
-(128, 'from contact page', '', '', 'my@sajdh.codsin', '3642879289786554534567', 'dfjskcmxkjekhds', '2020-07-10 00:00:00', 'open'),
-(129, 'hygh', '', '', 'sdvnk@kdnv.dosm', '238423480918', 'isdhcnsdjc', '2020-07-13 00:00:00', 'open'),
-(130, 'sdknf', '', '', 'sksdnf@sdlkm.sd', '8472349881', 'knfdsfsd', '2020-07-13 00:00:00', 'open'),
-(131, 'sdkkfmc', '', '', 'klsdmf@oskdfm.asdk', '384572842394', '<p>sdkfnsmdkdf</p>\r\n', '2020-07-14 00:00:00', 'open'),
-(132, 'hii', '', '', 'skdn@ksjsndf.saod', '1286371948', 'jsfnsdkf', '2020-07-14 00:00:00', 'open'),
-(133, 'sdkgj', '', '', 'sdklfn@ksfnd.wsdlkj', '2839473294', 'kjdvndkmsdl', '2020-07-14 00:00:00', 'open'),
-(134, 'dskjfn', '', '', 'skldn@ksdnf.woj', '293329849', 'kdjfmsdkfsd', '2020-07-14 00:00:00', 'open'),
-(135, 'asdkj', 'kasnd', 'klsnd', 'kns@ksdnf.sdko', '2834783', 'lsdkfmsld', '2020-07-15 20:07:15', 'open');
+INSERT INTO `inquiries` (`id`, `name`, `position`, `company`, `email`, `phone`, `message`, `product`, `created_at`, `mode`) VALUES
+(124, 'dfnskm', '', '', 'hjfdsk@hfjdkf.urefjdsks', '4538920', 'gfhdsjk', '', '2020-07-10 00:00:00', 'open'),
+(125, 'hgdfjk', '', '', 'dfuhsj@gdhfsj.ergufdjs', '348920', 'fdhjsk', '', '2020-07-10 00:00:00', 'open'),
+(126, 'hgdfjk', '', '', 'dfuhsj@gdhfsj.ergufdjs', '348920', 'fdhjsk', '', '2020-07-10 00:00:00', 'close'),
+(127, 'hgdfjk', '', '', 'dfuhsj@gdhfsj.ergufdjs', '348920', 'fdhjsk', '', '2020-07-10 00:00:00', 'open'),
+(128, 'from contact page', '', '', 'my@sajdh.codsin', '3642879289786554534567', 'dfjskcmxkjekhds', '', '2020-07-10 00:00:00', 'open'),
+(129, 'hygh', '', '', 'sdvnk@kdnv.dosm', '238423480918', 'isdhcnsdjc', '', '2020-07-13 00:00:00', 'open'),
+(130, 'sdknf', '', '', 'sksdnf@sdlkm.sd', '8472349881', 'knfdsfsd', '', '2020-07-13 00:00:00', 'open'),
+(131, 'sdkkfmc', '', '', 'klsdmf@oskdfm.asdk', '384572842394', '<p>sdkfnsmdkdf</p>\r\n', '', '2020-07-14 00:00:00', 'open'),
+(132, 'hii', '', '', 'skdn@ksjsndf.saod', '1286371948', 'jsfnsdkf', '', '2020-07-14 00:00:00', 'open'),
+(133, 'sdkgj', '', '', 'sdklfn@ksfnd.wsdlkj', '2839473294', 'kjdvndkmsdl', '', '2020-07-14 00:00:00', 'open'),
+(134, 'dskjfn', '', '', 'skldn@ksdnf.woj', '293329849', 'kdjfmsdkfsd', '', '2020-07-14 00:00:00', 'open'),
+(135, 'asdkj', 'kasnd', 'klsnd', 'kns@ksdnf.sdko', '2834783', 'lsdkfmsld', '', '2020-07-15 20:07:15', 'open'),
+(136, 'test name', 'skfj', 'iodjfds', 'sdkjf@skdjf.sdflih', '3298574857239', 'sjdkfnsdkjfdsf\r\nfrom index page', 'Title', '2020-07-20 10:25:29', 'open');
 
 -- --------------------------------------------------------
 
@@ -199,7 +225,8 @@ INSERT INTO `packing_specifications` (`id`, `country`, `specs`, `net_wt`, `gross
 (3, 'hd', 'sjd', 'jdsfn', 'kjdsfn', 'sksdjfnf', 'sksjdvn', 'skdjn', 'skdjfn', 1),
 (4, 'ghb', 'fghj', 'rdtfghytre', 'qweyghh', 'qwer', 'qweriu', 'qwertyuuytq', 'qwertyui', 4),
 (5, 'djgf', 'odjfv', 'kncxv', 'ojdfmvkd', 'kldsnv', 'osdkjv', 'lklssjvn', 'kldjvndc', 5),
-(6, 'India', 'kasdk', 'ksdfnjksm', 'ksdnf', 'skdnf', 'ksdnf', 'ksdnf', 'ksfn', 15);
+(6, 'India', 'kasdk', 'ksdfnjksm', 'ksdnf', 'skdnf', 'ksdnf', 'ksdnf', 'ksfn', 15),
+(7, 'djfghdsfj', 'wjdsf', 'ojsdkj', 'sdjfdjs', 'sdkjfds', 'sjsdfsdj', 'jsdfnsdj', 'sjdf', 13);
 
 -- --------------------------------------------------------
 
@@ -233,16 +260,12 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `subcategory_id`, `title`, `description`, `varieties`, `color`, `size`, `weight`, `tss`, `calender`, `containercapacity`, `incoterms`, `paymenterms`, `certifications`, `image`, `display_on_home`, `position`) VALUES
 (4, 1, 'New title', 'update', 'update', 'update', 'update', 'update', 'update', '1,2,3,4,7,8,9,11', 'update', 'update', 'dfjckupdate', 'fdjkupdate', 'storage/159457515302.jpg', 'on', 5),
-(5, 2, 'House', 'description', 'hjbj', 'dskjvn', 'kjcvn', 'ksncvmc', 'ckvn', '2,4,9,11', 'jfhdskjfs', 'ijldsfksd', 'lksjdvkdn', 'lksddjvmdk', 'storage/159461458303.jpg', 'on', 2),
 (6, 3, 'sdkfjgf', 'kcvxnm', 'dfkbnfo', 'dgfjv', 'jdjfkvn', 'jdjnv', 'dknv', '1,4,8,11', 'ojdfs', 'dfkfnv', 'dkfvn', 'dkfng', 'storage/159461478202.jpg', 'off', 0),
-(7, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461474401.jpg', '', 0),
-(8, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461474901.jpg', 'on', 1),
-(9, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461475101.jpg', '', 0),
-(11, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461475401.jpg', '', 0),
-(12, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461479701.jpg', 'on', 4),
-(13, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461475701.jpg', 'off', 0),
-(14, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,5,9,12', 'odsijfds', 'osidjifm', 'oijdfs', 'osisdifj', 'storage/159461476501.jpg', '', 0),
-(15, 1, 'Title', 'New Description', 'asjdns', 'black', '12x23', '123', 'sdkdsf', '2,5,9', 'ldkfjgkl', 'lmgdklv', '<ul>\r\n	<li>sldfm,lvsf</li>\r\n	<li>ldkmgfd</li>\r\n</ul>\r\n', '<ul>\r\n	<li>skedgnfksd</li>\r\n	<li>jsdfnd</li>\r\n</ul>\r\n', 'storage/159472230601.jpg', 'on', 3);
+(12, 2, 'isdhfj', 'odpidfjv', 'odfdfjv', 'oidjf', 'oskdk', 'ojv', 'oissdjf', '1,3,5,8', 'odsijfds', 'osidjifm', '<p>skjfndgsk</p>\r\n\r\n<p>idahsjkd</p>\r\n', '<p>some text</p>\r\n', 'storage/159461479701.jpg', 'on', 4),
+(15, 1, 'Title', 'New Description', 'asjdns', 'black', '12x23', '123', 'sdkdsf', '2,5,9', 'ldkfjgkl', 'lmgdklv', '<ul>\r\n	<li>sldfm,lvsf</li>\r\n	<li>ldkmgfd</li>\r\n</ul>\r\n', '<ul>\r\n	<li>skedgnfksd</li>\r\n	<li>jsdfnd</li>\r\n</ul>\r\n', 'storage/159472230601.jpg', 'on', 3),
+(16, 5, 'sjdfdsj', 'jksdfnsdkjfn', 'kjsdngjdkn', 'dskjsdfnsdjkfn', 'skjndjkn', 'kjnsdj', 'kjsdfn', '1,9', 'zdfkjgsdk', 'kdjfgmkdslf', '<p>sdklfjmdsklf</p>\r\n', '<p>kdlfgmdfklgmdf</p>\r\n', 'storage/1595054920download.png', NULL, NULL),
+(17, 2, 'skdfmsk', 'kgiormkfm', 'dfklmgfm', 'orietuitu', 'ierjeirm', 'odmfdksfm', 'ksdfmds', '1,4,8,11,12', 'kjsdiofjerij', 'oiewjrewiorj', '<p>rijig</p>\r\n\r\n<p>esoirjs</p>\r\n', '<p>wiojmekf</p>\r\n\r\n<p>eofjemf</p>\r\n', 'storage/1595130204download.png', NULL, NULL),
+(18, 2, 'ifdhgj', 'oksjdngdf', 'kldfdmv', 'kfndb', 'kdfdjng', 'kjnd', 'knd', '1,4,7,10,12', 'oidjfsdi', 'kojfgmdk', '<p>okjdmkd</p>\r\n', '<p>oskjdsmdkf</p>\r\n', 'storage/1595136835download.png', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -268,7 +291,8 @@ INSERT INTO `slides` (`id`, `title`, `description`, `file`, `type`) VALUES
 (7, '', '', 'storage/1594316719kiwi-pic.jpg', 'image'),
 (8, 'asn', 'aksnfds', 'storage/159472236801.jpg', 'image'),
 (9, '', '', 'storage/1594786415Rakotzbrücke.jpg', 'image'),
-(11, '', '', 'storage/1594822235img_1.jpg', 'image');
+(11, '', '', 'storage/1594822235img_1.jpg', 'image'),
+(12, '', '', 'storage/1595224798Lesson 1. Course Introduction.mp4', 'video');
 
 -- --------------------------------------------------------
 
@@ -287,9 +311,13 @@ CREATE TABLE `subcategories` (
 --
 
 INSERT INTO `subcategories` (`id`, `name`, `category_id`) VALUES
-(1, 'wsgyvf', 1),
 (2, 'frybdp', 1),
-(3, 'cfthb', 2);
+(3, 'cfthb', 2),
+(4, 'New Subcategory', 3),
+(5, 'New sub category', 2),
+(6, 'aldskjfds', 2),
+(7, 'skdljf', 3),
+(8, 'asodokjflds', 3);
 
 -- --------------------------------------------------------
 
@@ -336,6 +364,12 @@ ALTER TABLE `categories`
 -- Indexes for table `feedbacks`
 --
 ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -394,25 +428,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `feedbacks`
 --
 ALTER TABLE `feedbacks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `inquiries`
 --
 ALTER TABLE `inquiries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 
 --
 -- AUTO_INCREMENT for table `months`
@@ -424,25 +464,25 @@ ALTER TABLE `months`
 -- AUTO_INCREMENT for table `packing_specifications`
 --
 ALTER TABLE `packing_specifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `slides`
 --
 ALTER TABLE `slides`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `subcategories`
 --
 ALTER TABLE `subcategories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `subscribers`
