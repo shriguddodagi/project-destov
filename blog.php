39604b5db711fbbdfa3d04371f455b2dc7769792
kaushalkoladiya <kaushalkoladiya123@gmail.com>
2020-07-29 16:09:41 +0530

    add previous and next button

--- blog.php
+++ blog.php
@@ -1,13 +1,15 @@
 <?php
 include_once('./includes/header.php');
-
 $blogId = $_GET['blog'];
-$query = "SELECT * FROM `blogs` WHERE id='$blogId'";
-$blog = mysqli_fetch_array(mysqli_query($cn, $query));
+$blog = mysqli_fetch_array(mysqli_query($cn, "SELECT * FROM `blogs` WHERE id='$blogId' LIMIT 1"));
 
 if(!count($blog)) {
   header('Location: blogs.php');
 }
+
+$prev = mysqli_fetch_array(mysqli_query($cn, "SELECT id FROM `blogs` WHERE id<$blogId ORDER BY id DESC LIMIT 1"))['id'];
+$next = mysqli_fetch_array(mysqli_query($cn, "SELECT id FROM `blogs` WHERE id>$blogId ORDER BY id ASC LIMIT 1"))['id'];
+
 ?>
 
 
@@ -26,6 +28,12 @@ if(!count($blog)) {
       </div>
     </div>
     <p><i class='fa fa-lg fa-calendar' style='color: #17bed2;'></i> &nbsp <?php echo date('jS F, Y', strtotime($blog['created_at'])) ?></p>
+    <div class="row">
+      <div class="col-md-12" style="display: flex; justify-content: center;">
+        <a href="blog.php?blog=<?php if(isset($prev)) { echo $prev; } ?>" <?php if(!isset($prev)) { echo "disabled"; } ?> class="btn-info btn text-uppercase margin-r-20">Previous</a>
+        <a href="blog.php?blog=<?php if(isset($next)) { echo $next; } ?>" <?php if(!isset($next)) { echo "disabled"; } ?> class="btn-info btn text-uppercase margin-l-20">Next</a>
+      </div>
+    </div>
   </div>
 </div>
 
